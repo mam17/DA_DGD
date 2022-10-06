a7e9e165923ec8db92fe3cdc360d0869f334896b
Longg <duongthanhlong1710@gmail.com>
2022-10-06 11:25:21 +0700

    Update Cart.php

--- DA/app/Models/Cart.php
+++ DA/app/Models/Cart.php
@@ -11,6 +11,7 @@ class Cart extends Model
     public $products = null;
     public $totalPrice = 0;
     public $totalQuanty = 0;
+
     public function __construct($cart)
     {
         if($cart)
@@ -20,9 +21,15 @@ class Cart extends Model
             $this->totalQuanty= $cart->totalQuanty;
         }
     }
+
     public function AddCart($product, $id)
     {
-        $newProduct = ['quanty' =>0,'price'=>$product ->price, 'productInfo'=>$product];
+        $newProduct = [
+            'quanty' =>0,
+            'price'=>$product->discount ? $product->discount : $product->price, 
+            'productInfo'=>$product
+        ];
+    
         if($this ->products)
         {
             if(array_key_exists($id, $this ->products))
@@ -33,24 +40,34 @@ class Cart extends Model
         $newProduct['quanty']++;
         $newProduct['price']= $newProduct['quanty'] * $product->price;
         $this->products[$id]= $newProduct;
-        $this->totalPrice += $product->price;
+        $this->totalPrice += $product->discount ? $product->discount : $product->price;
         $this->totalQuanty++;
 
     }
+
     public function DeleteItemCart($id){
         $this->totalQuanty -=$this->products[$id]['quanty'];
         $this->totalPrice -= $this ->products[$id]['price'];
         unset($this->products[$id]);
     }
+
     public function UpdateItemCart($id, $quanty)
     {
         $this->totalQuanty-=$this->products[$id]['quanty'];
         $this->totalPrice-=$this->products[$id]['price'];
         
         $this->products[$id]['quanty']= $quanty;
-        $this->products[$id]['price']= $quanty * $this->products[$id]['productInfo']->price;
+
+        if($this->products[$id]['productInfo']->discount != 0){
+            $this->products[$id]['price']= $quanty * $this->products[$id]['productInfo']->discount;
+        }
+        else{
+            $this->products[$id]['price']= $quanty * $this->products[$id]['productInfo']->price;
+        }
 
         $this->totalQuanty+=$this->products[$id]['quanty'];
-        $this->totalPrice+=$this->products[$id]['price'];
+
+        $this->totalPrice += $this->products[$id]['price'] ;
+        
     }
 }
